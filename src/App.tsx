a773ef04d3249c98e0e9bd7a16ab89631d109871
imarunp <arcanearun@gmail.com>
2023-11-09 11:17:13 +0545

    Top Navbar Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import Routers from "./routes/Routers";
-import "./App.css";
+import Routers from './routes/Routers';
+import './App.css';
 // import { MapProvider } from "react-map-gl/maplibre";
 
 function App() {
   return (
-    <>
+    <div className='bg-gray-200'>
       {/* <MapProvider> */}
       <Routers />
       {/* </MapProvider> */}
-    </>
+    </div>
   );
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,28 +1,30 @@
 // #Note Use Tailewind classes anywhere already configured
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import NavBar from '../components/commoncomp/NavBar';
 
 export default function Login() {
   const navigate = useNavigate();
   return (
-    <div>
-      <h1 className="text-1xl ">Login page</h1>
+    <div className='mx-5'>
+      <NavBar />
+      <h1 className='text-1xl '>Login page</h1>
 
-      <div className="flex">
+      <div className='flex'>
         <button
-          className="text-3xl font-bold underline text-blue-600"
-          onClick={() => navigate("/map")}
+          className='text-3xl font-bold underline text-blue-600'
+          onClick={() => navigate('/map')}
         >
           Go To Map
         </button>
         <button
-          className="text-3xl font-bold underline text-red-900"
-          onClick={() => navigate("/dashboard")}
+          className='text-3xl font-bold underline text-red-900'
+          onClick={() => navigate('/dashboard')}
         >
           Dashborad
         </button>
         <button
-          className="text-3xl font-bold underline text-green-600"
-          onClick={() => navigate("/register")}
+          className='text-3xl font-bold underline text-green-600'
+          onClick={() => navigate('/register')}
         >
           Register
         </button>

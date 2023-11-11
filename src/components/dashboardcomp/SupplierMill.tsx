dda2e8e8a092be98a6e825649311d7bb430b7dfb
imarunp <arcanearun@gmail.com>
2023-11-11 11:42:39 +0545

    Table UI Completed

--- src/components/dashboardcomp/SupplierMill.tsx
+++ src/components/dashboardcomp/SupplierMill.tsx
@@ -1,10 +1,10 @@
-import Dropdown from "../commoncomp/Dropdown";
-import Layout from "../commoncomp/Layout";
-import TableExample from "./Table";
+import Dropdown from '../commoncomp/Dropdown';
+import Layout from '../commoncomp/Layout';
+import SimpleTable from './Table';
 // import MapSection from '../../pages/MapSection';
 
 function SupplierMill() {
-  const optionsReporting = ["Metric", "Mill Supplier"];
+  const optionsReporting = ['Metric', 'Mill Supplier'];
 
   return (
     <Layout>
@@ -12,28 +12,28 @@ function SupplierMill() {
       {/* <div className='h-32'>
         <MapSection />
       </div> */}
-      <div className="mx-4 my-3">
-        <Dropdown options={optionsReporting} placeholder="Metric" />
+      <div className='mx-4 my-3'>
+        <Dropdown options={optionsReporting} placeholder='Metric' />
       </div>
-      <div className="space-x-4  flex">
-        <div className="rounded-sm bg-white w-1/4 py-3 px-4">
-          <h1 className="font-semibold">Supply Mill Region</h1>
+      <div className='space-x-4  flex'>
+        <div className='rounded-sm bg-white w-1/4 py-3 px-4'>
+          <h1 className='font-semibold'>Supply Mill Region</h1>
           {/* Add piechart here */}
         </div>
-        <div className="rounded-sm bg-white w-1/4 py-3 px-4">
-          <h1 className="font-semibold">Supplier Type</h1>
+        <div className='rounded-sm bg-white w-1/4 py-3 px-4'>
+          <h1 className='font-semibold'>Supplier Type</h1>
           {/* Add piechart here */}
         </div>
-        <div className="rounded-sm bg-white w-1/4 py-3 px-4">
-          <h1 className="font-semibold">RSPO Certified</h1>
+        <div className='rounded-sm bg-white w-1/4 py-3 px-4'>
+          <h1 className='font-semibold'>RSPO Certified</h1>
           {/* Add piechart here */}
         </div>
-        <div className="rounded-sm bg-white w-1/4 py-3 px-4">
-          <h1 className="font-semibold">Supplier Risk</h1>
+        <div className='rounded-sm bg-white w-1/4 py-3 px-4'>
+          <h1 className='font-semibold'>Supplier Risk</h1>
           {/* Add piechart here */}
         </div>
       </div>
-      <TableExample />
+      <SimpleTable />
     </Layout>
   );
 }
--- src/components/dashboardcomp/Table.tsx
+++ src/components/dashboardcomp/Table.tsx
@@ -1,4 +1,85 @@
-function Table() {
-  return <div>Table</div>;
+const theads = [
+  'Mill Name',
+  'Region',
+  'Country',
+  'Group Company',
+  'Company',
+  'RSPO Certified',
+];
+const data = [
+  [
+    'Batu Lintang',
+    'Kedah',
+    'Malaysia',
+    'Kuala Lumpur Kepong Behad',
+    'Kuala Lambur Kepong Behad (KLK Batu Lintang)',
+    'IP',
+    <a className='text-blue-600 underline' href='#'>
+      Details
+    </a>,
+  ],
+  [
+    'Batu Lintang',
+    'Kedah',
+    'Malaysia',
+    'Kuala Lumpur Kepong Behad',
+    'Kuala Lambur Kepong Behad (KLK Batu Lintang)',
+    'IP',
+    <a className='text-blue-600 underline' href='#'>
+      Details
+    </a>,
+  ],
+  [
+    'Batu Lintang',
+    'Kedah',
+    'Malaysia',
+    'Kuala Lumpur Kepong Behad',
+    `Kuala Lambur Kepong Behad (KLK Batu Lintang)`,
+    'IP',
+    <a className='text-blue-600 underline' href='#'>
+      Details
+    </a>,
+  ],
+
+  // Add more rows as needed
+];
+
+function TableHeaders({ headers }) {
+  return (
+    <thead className='text-left bg-[#FAFAFA] shadow'>
+      <tr>
+        {headers.map((header, index) => (
+          <th key={index} className='py-2 px-4 border-b'>
+            {header}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
 }
-export default Table;
+
+function Table({ headers, data }) {
+  return (
+    <div className='bg-gray-100 py-6'>
+      <table className='min-w-full bg-white '>
+        <TableHeaders headers={headers} />
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex} className='py-5 px-4 border-b'>
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function SimpleTable() {
+  return <Table headers={theads} data={data} />;
+}
+export default SimpleTable;

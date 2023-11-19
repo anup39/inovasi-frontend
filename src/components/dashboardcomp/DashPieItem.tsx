2f4ce2e824f892813fbb8243749d30cd71d88dbf
Anup Dahal <adahal39@gmail.com>
2023-11-19 21:16:08 +0545

    Refactored pie chart

--- src/components/dashboardcomp/DashPieItem.tsx
+++ src/components/dashboardcomp/DashPieItem.tsx
@@ -1,45 +1,59 @@
+import React, { useState } from "react";
 import "../../css/dashborad/DashBoardItem.css";
 import PieChart from "./PieChart";
 import pie_chart_api from "../../utils/constants";
 
+interface Item {
+  id: number;
+  name: string;
+  selected: boolean;
+}
+
 function DashPieItem() {
+  const [items, setItems] = useState<Item[]>([
+    {
+      id: 1,
+      name: "Supply Base Region",
+      selected: false,
+    },
+    {
+      id: 2,
+      name: "Supplier Type",
+      selected: false,
+    },
+    {
+      id: 3,
+      name: "RSPO Certified",
+      selected: false,
+    },
+  ]);
+
   return (
-    <div className="flex  flex-col w-1/2 items-center py-7 gap-6 lg:flex-row">
-      <div className="bg-white dashItems ">
-        <div>
-          <div className="p-2">
-            <h1 className="text-black font-bold">Supply Base Region</h1>
-            <PieChart
-              data={pie_chart_api.supplybaseregion}
-              width_={180}
-              height_={180}
-            />
+    <div className="flex flex-col w-1/2 items-center py-7 gap-6 lg:flex-row">
+      {items.map((item) => (
+        <div key={item.id} className="bg-white dashItems">
+          <div>
+            <div className="p-2">
+              <h1 className="text-black font-bold">{item.name}</h1>
+              <PieChart
+                data={
+                  item.name === "Supply Base Region"
+                    ? pie_chart_api.supplybaseregion
+                    : item.name === "Supplier Type"
+                    ? pie_chart_api.suppliertype
+                    : item.name === "RSPO Certified"
+                    ? pie_chart_api.rspocertified
+                    : []
+                }
+                width_={180}
+                height_={180}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="bg-white  dashItems">
-        <div className="p-2">
-          <h1 className="text-black font-bold">Supplier Type</h1>
-          {/* Pie Chart Here */}
-          <PieChart
-            data={pie_chart_api.suppliertype}
-            width_={180}
-            height_={180}
-          />
-        </div>
-      </div>
-      <div className="bg-white  dashItems ">
-        <div className="p-2 ">
-          <h1 className="text-black font-bold">RSPO Certified</h1>
-
-          <PieChart
-            data={pie_chart_api.rspocertified}
-            width_={180}
-            height_={180}
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
+
 export default DashPieItem;

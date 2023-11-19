b87a39db25ec113e1c3bf1182aa299b3e2203635
Anup Dahal <adahal39@gmail.com>
2023-11-19 20:37:32 +0545

    refactor

--- src/components/dashboardcomp/DashPieItem.tsx
+++ src/components/dashboardcomp/DashPieItem.tsx
@@ -1,14 +1,14 @@
-import '../../css/dashborad/DashBoardItem.css';
-import PieChart from './PieChart';
-import pie_chart_api from '../../utils/constants';
+import "../../css/dashborad/DashBoardItem.css";
+import PieChart from "./PieChart";
+import pie_chart_api from "../../utils/constants";
 
 function DashPieItem() {
   return (
-    <div className='flex  flex-col w-1/2 items-center py-7 gap-6 lg:flex-row'>
-      <div className='bg-white dashItems '>
+    <div className="flex  flex-col w-1/2 items-center py-7 gap-6 lg:flex-row">
+      <div className="bg-white dashItems ">
         <div>
-          <div className='p-2'>
-            <h1 className='text-black font-bold'>Supply Base Region</h1>
+          <div className="p-2">
+            <h1 className="text-black font-bold">Supply Base Region</h1>
             <PieChart
               data={pie_chart_api.supplybaseregion}
               width_={180}
@@ -17,9 +17,9 @@ function DashPieItem() {
           </div>
         </div>
       </div>
-      <div className='bg-white  dashItems'>
-        <div className='p-2'>
-          <h1 className='text-black font-bold'>Supplier Type</h1>
+      <div className="bg-white  dashItems">
+        <div className="p-2">
+          <h1 className="text-black font-bold">Supplier Type</h1>
           {/* Pie Chart Here */}
           <PieChart
             data={pie_chart_api.suppliertype}
@@ -28,9 +28,9 @@ function DashPieItem() {
           />
         </div>
       </div>
-      <div className='bg-white  dashItems '>
-        <div className='p-2 '>
-          <h1 className='text-black font-bold'>RSPO Certified</h1>
+      <div className="bg-white  dashItems ">
+        <div className="p-2 ">
+          <h1 className="text-black font-bold">RSPO Certified</h1>
           {/* Pie Chart Here */}
           <PieChart
             data={pie_chart_api.rspocertified}

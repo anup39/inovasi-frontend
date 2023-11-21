8acd119a4bf8da78191483428a00921ba116e2a1
Anup Dahal <adahal39@gmail.com>
2023-11-21 16:37:39 +0545

    local changes

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,11 @@
-import Login from '../components/login/Login';
-// import Register from '../components/login/Register';
+// import Login from "../components/login/Login";
+import Register from "../components/login/Register";
 
 function LoginPage() {
   return (
     <div>
-      {/* <Register /> */}
-      <Login />
+      <Register />
+      {/* <Login /> */}
     </div>
   );
 }

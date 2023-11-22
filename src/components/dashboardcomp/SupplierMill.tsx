a520b3ca0b697a243c5799e93bcc0b86d1645470
Anup Dahal <adahal39@gmail.com>
2023-11-22 18:02:54 +0545

    Table completed

--- src/components/dashboardcomp/SupplierMill.tsx
+++ src/components/dashboardcomp/SupplierMill.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AddLayerAndSourceToMap from "../../maputils/AddSourceAndLayer";
 import MapSection from "../../pages/MapSection";
 import PieChartComp from "./PieChartComp";
@@ -8,6 +8,7 @@ import TableComp from "./TableComp";
 // import MapSection from '../../pages/MapSection';
 import { Map } from "maplibre-gl"; // Import 'Map' from 'maplibre-gl'
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const items = [
   {
@@ -36,6 +37,9 @@ interface SupplierMillProps {
 }
 
 const SupplierMill: React.FC<SupplierMillProps> = ({ map, onSetMap }) => {
+  const [tableColumn, settableColumn] = useState([]);
+  const [tableData, settableData] = useState([]);
+
   const selectedDataFormat = useSelector(
     (state) => state.displaySettings.selectedDataFormat
   );
@@ -63,13 +67,27 @@ const SupplierMill: React.FC<SupplierMillProps> = ({ map, onSetMap }) => {
     }
   }, [map]);
 
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_DASHBOARD_URL}/table-column/mill/`)
+      .then((res) => {
+        settableColumn(res.data.columns);
+      });
+
+    axios.get(`${import.meta.env.VITE_API_DASHBOARD_URL}/mill/`).then((res) => {
+      settableData(res.data);
+    });
+  }, []);
+
   return (
     <Layout>
       <div className="mt-4 mb-2">
         <MapSection map={map} onSetMap={onSetMap} component={"mill"} />
       </div>
       {selectedDataFormat && selectedDataFormat === "Supplier Mill" ? (
-        <TableComp />
+        <>
+          <TableComp tableColumn={tableColumn} tableData={tableData} />
+        </>
       ) : (
         // <div className="flex flex-col w-1/2 items-center justify  py-7 gap-8 lg:flex-row">
         <div className="flex">

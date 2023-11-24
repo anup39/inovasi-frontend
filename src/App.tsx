972e6ad9b3b6136a031456707644f716e93ed848
Anup Dahal <adahal39@gmail.com>
2023-11-24 17:39:00 +0545

    Removed unecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Routers from "./routes/Routers";
 import "./App.css";
 import { useState } from "react";
 import { Map } from "maplibre-gl";
-import Toast from "./components/commoncomp/Toast";
 
 function App() {
   const [map, setMap] = useState<Map | null>(null);

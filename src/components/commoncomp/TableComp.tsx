8b380742e6a475fc84dbe88854a738d1a5ec78ae
Anup Dahal <adahal39@gmail.com>
2023-11-24 16:57:28 +0545

    Fixed typings

--- src/components/commoncomp/TableComp.tsx
+++ src/components/commoncomp/TableComp.tsx
@@ -1,21 +1,26 @@
 import Box from "@mui/material/Box";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import maplibregl, { Map } from "maplibre-gl"; // Import 'Map' type from 'maplibre-gl'
+import maplibregl, { Map, LngLatBoundsLike } from "maplibre-gl";
 
-const getGeoJSON = (data, indices, component) => {
-  const filteredData = [];
-  indices.forEach((index) => {
-    data.forEach((item) => {
-      if (item.id == index) {
-        filteredData.push(item);
-      }
-    });
-  });
+interface DataItem {
+  id: number;
+  [key: string]: any; // Any other key can have any value
+}
+
+interface DataGridDemoProps {
+  tableColumn: GridColDef[];
+  tableData: DataItem[];
+  map: Map | null;
+  component: string;
+}
+
+const getGeoJSON = (data: DataItem[], indices: number[], component: string) => {
+  const filteredData = data.filter((item) => indices.includes(item.id));
+
   const features = filteredData.map((item) => {
-    // const { mill_lat, mill_long, ...properties } = item;
-    const long = component + "_" + "long";
-    const lat = component + "_" + "lat";
-    console.log(long, lat);
+    const long = `${component}_long`;
+    const lat = `${component}_lat`;
+
     return {
       type: "Feature",
       properties: {},
@@ -38,40 +43,39 @@ export default function DataGridDemo({
   tableData,
   map,
   component,
-}) {
-  const columns: GridColDef[] = tableColumn;
-
-  const handleonRowSelectionModelChange = (rows, details) => {
-    if (rows.length > 0) {
-      const geojson = getGeoJSON(tableData, rows, component);
+}: DataGridDemoProps) {
+  const handleonRowSelectionModelChange = (rows: number[]) => {
+    if (rows.length > 0 && map) {
+      const geojson: any = getGeoJSON(tableData, rows, component);
       const padding = { top: 25, bottom: 25, left: 25, right: 25 };
-      const bounds = new maplibregl.LngLatBounds();
-      geojson.features.forEach((feature) => {
+      const bounds = new maplibregl.LngLatBounds() as LngLatBoundsLike;
+
+      geojson.features.forEach((feature: any) => {
+        // @ts-ignore
         bounds.extend(feature.geometry.coordinates);
       });
-      if (map) {
-        if (map.getSource("point-table") && map.getLayer("point-table-layer")) {
-          const source = map.getSource("point-table");
-          source.setData(geojson);
-          map.fitBounds(bounds, { padding });
-          map.setLayoutProperty("point-table-layer", "visibility", "visible");
-        }
+
+      if (map.getSource("point-table") && map.getLayer("point-table-layer")) {
+        const source: any = map.getSource("point-table");
+        source.setData(geojson);
+        map.fitBounds(bounds, { padding });
+        map.setLayoutProperty("point-table-layer", "visibility", "visible");
       }
-    }
-    if (rows.length == 0) {
-      if (map) {
-        if (map.getSource("point-table") && map.getLayer("point-table-layer")) {
-          map.setLayoutProperty("point-table-layer", "visibility", "none");
-        }
-      }
+    } else if (
+      map &&
+      map.getSource("point-table") &&
+      map.getLayer("point-table-layer")
+    ) {
+      map.setLayoutProperty("point-table-layer", "visibility", "none");
     }
   };
+
   return (
     <Box sx={{ height: 350, width: "100%" }}>
       <DataGrid
         hideFooter={false}
         rows={tableData}
-        columns={columns}
+        columns={tableColumn}
         initialState={{
           pagination: {
             paginationModel: {
@@ -83,7 +87,7 @@ export default function DataGridDemo({
         checkboxSelection={true}
         disableRowSelectionOnClick
         disableColumnSelector={false}
-        // onCellClick={handleOnCellClick}
+        // @ts-ignore
         onRowSelectionModelChange={handleonRowSelectionModelChange}
       />
     </Box>

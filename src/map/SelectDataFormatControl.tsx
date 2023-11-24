f70527014611d7eff8d528ff5e1063b23e1f97b7
Anup Dahal <adahal39@gmail.com>
2023-11-24 18:35:33 +0545

    Typing in control

--- src/map/SelectDataFormatControl.tsx
+++ src/map/SelectDataFormatControl.tsx
@@ -1,20 +1,34 @@
-import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
 import Dropdown from "../components/commoncomp/Dropdown";
 import { store } from "../store";
 import { Provider } from "react-redux";
+import { Map } from "maplibre-gl";
 
-const optionsReporting = ["Supplier Mill", "Metric"];
+const optionsReporting: string[] = ["Supplier Mill", "Metric"];
+
 export default class SelectDataFormatControl {
-  onAdd(map) {
+  private _map: Map | undefined;
+  private _container: HTMLDivElement | null = null;
+
+  onAdd(map: Map) {
     this._map = map;
     this._container = document.createElement("div");
-    this._container.className = "maplibregl-ctrl ";
-    ReactDOM.createRoot(this._container).render(
+    this._container.className = "maplibregl-ctrl";
+
+    if (this._container) {
+      this.renderReactComponent();
+    }
+
+    return this._container;
+  }
+
+  private renderReactComponent() {
+    render(
       <Provider store={store}>
         <Dropdown options={optionsReporting} placeholder="Supplier Mill" />
-      </Provider>
+      </Provider>,
+      this._container
     );
-    return this._container;
   }
 
   onRemove() {

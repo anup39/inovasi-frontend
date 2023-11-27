ec880be45643595a8bdda5a32f6926a1625dbcd8
Anup Dahal <adahal39@gmail.com>
2023-11-27 20:09:28 +0545

    moved the typing issue function

--- src/components/commoncomp/TableComp.tsx
+++ src/components/commoncomp/TableComp.tsx
@@ -9,7 +9,56 @@ import maplibregl, {
 import calculateBoundingBoxPolygonfromGeojson from "../../maputils/calculateBoundingBoxPolygonfromGeojson";
 import getGeojsonFromwktTableWithGeom from "../../maputils/getGeojsonFromwktTableWithGeom";
 // @ts-ignore
-import getGeojsonFromwktTableWithLatlong from "../../maputils/getGeojsonFromwktTablewithLatlong";
+// import getGeojsonFromwktTableWithLatlong from "../../maputils/getGeojsonFromwktTablewithLatlong";
+
+interface DataItem {
+  id: number;
+  [key: string]: unknown;
+}
+
+type PointFeature = {
+  type: "Feature";
+  properties: Record<string, string>;
+  geometry: {
+    type: "Point";
+    coordinates: [number, number];
+  };
+};
+
+const getGeojsonFromwktTableWithLatlong = (
+  data: DataItem[],
+  indices: number[],
+  component: string
+): GeoJSON.FeatureCollection<GeoJSON.Geometry, GeoJSON.GeoJsonProperties> => {
+  const filteredData = data.filter((item) => indices.includes(item.id));
+
+  const features: PointFeature[] = filteredData.map((item) => {
+    const longKey = `${component}_long`;
+    const latKey = `${component}_lat`;
+
+    const long = parseFloat(String(item[longKey as keyof typeof item]));
+    const lat = parseFloat(String(item[latKey as keyof typeof item]));
+    return {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Point",
+        coordinates: [long, lat],
+      },
+    };
+  });
+
+  const geojson: GeoJSON.FeatureCollection<
+    GeoJSON.Geometry,
+    GeoJSON.GeoJsonProperties
+  > = {
+    type: "FeatureCollection",
+    features: features,
+  };
+  return geojson;
+};
+
+// export default getGeojsonFromwktTableWithLatlong;
 
 interface DataGridDemoProps {
   tableColumn: GridColDef[];

c69843158d66a3a2c95cb2f727990611c7b4723a
imarunp <arcanearun@gmail.com>
2023-12-01 10:49:00 +0545

    Sidebar placement done

--- src/components/commoncomp/Layout.tsx
+++ src/components/commoncomp/Layout.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import NavBar from "./NavBar";
-// import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 function Layout({ children }: LayoutProps) {
+  const [sidebarOpened, setSidebarOpened] = useState(true);
   return (
-    <>
-      {/* <Sidebar /> */}
-      <NavBar />
-      {children}
-    </>
+    <div className="flex">
+      <div className="">
+        <Sidebar
+          setSidebarOpened={setSidebarOpened}
+          sidebarOpened={sidebarOpened}
+        />
+      </div>
+      <div className="flex-1">
+        <NavBar />
+        {children}
+      </div>
+    </div>
   );
 }
 

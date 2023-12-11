8de5f80765fac08a317ff0d40f99c223ce269fb4
kavya123456 <kavya.timilsina1230@gmail.com>
2023-12-11 13:46:02 +0545

    working on upload image part

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,8 @@
 import { Checkbox } from '@mui/material';
 import '../css/login/Login.css';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import PhoneInput from 'react-phone-number-input';
 import flags from 'react-phone-number-input/flags';
-import 'react-phone-number-input/style.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -12,48 +12,54 @@ function Register() {
     <div className=' flex max-w-[1920px] m-auto'>
       <div className='sm:w-3/4'>
         <img className='scale-75 pt-4 pl-5' src='inovasilogo.svg' alt='' />
-        <h2 className='text-footerHeading text-center font-bold'>Welcome!</h2>
-        <h1 className='text-[24px] text-center font-bold'>Create an account</h1>
-        <div className='bg-lightGray mt-6 mb-[50px] max-w-[826px] m-auto'>
+        <h2 className='text-footerHeading text-center mt-6 sm:mt-0 font-bold'>
+          Welcome!
+        </h2>
+        <h1 className='text-[24px] text-center font-bold mt-6 sm:mt-0'>
+          Create an account
+        </h1>
+        <div className='bg-lightGray mt-6 mb-[50px] max-w-[300] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[700px] xl:max-w-[750px] 2xl:max-w-[826px] m-auto'>
           <form>
-            <div className='flex justify-center pt-12 '>
-              <label className='w-24 h-24 lg:w-36 lg:h-36 bg-gray-100 cursor-pointer flex justify-center items-center border-2 border-gray-200 rounded-full'>
-                <div className='text-center space-y-2'>
-                  <p>+</p>
-                  <p>Upload</p>
-                </div>
-              </label>
+            <div className='flex relative justify-center pt-12 '>
+              <input
+                type='file'
+                accept='image'
+                className='border rounded-full w-32 h-32'
+              />
+              <button
+                type='button'
+                className='absolute top ml-20 w-10 h-10 rounded-full bottom-1  cursor-pointer bg-lightGray shadow-md '
+              >
+                <AddAPhotoIcon />
+              </button>
             </div>
-            <div className='flex flex-col px-[34px]'>
-              <label className='font-semibold mb-2'> Name </label>
+            <div className='flex flex-col px-[34px] gap-4 sm:gap-1.5'>
+              <label className='font-semibold'> Name </label>
               <input placeholder='Full name' className='p-[10px] rounded-md' />
-              <label className='font-semibold mb-2 mt-4'> Company </label>
+              <label className='font-semibold'> Company </label>
               <input
                 placeholder='Company Name'
                 className='p-[10px] rounded-md'
               />
-              <label className='font-semibold mb-2 mt-4'> Email </label>
+              <label className='font-semibold '> Email </label>
               <input
                 placeholder='you@company.com'
                 className='p-[10px] rounded-sm'
               />
-              <label className='font-semibold mb-2 mt-4'> Phone Number </label>
+              <label className='font-semibold'> Phone Number </label>
               <PhoneInput
-                className='p-[10px] rounded-sm w-full bg-white mr-[60px]'
+                className=' bg-white'
                 flags={flags}
                 country={'us'}
                 value={phone}
                 onChange={(phone: string) => setPhone(phone)}
               />
-              <label className='font-semibold mb-2 mt-4'>
-                {' '}
-                Confirm Passwod{' '}
-              </label>
+              <label className='font-semibold'> Confirm Passwod </label>
               <input
                 placeholder='Confirm Password'
                 className='p-[10px] rounded-sm'
               />
-              <div className='flex items-center pt-8'>
+              <div className='flex items-center'>
                 <Checkbox />
                 <p>
                   I agree to the{' '}
@@ -82,10 +88,10 @@ function Register() {
                   </span>
                 </p>
               </div>
-              <button className='mr-[60px] h-[40px] text-white w-full mt-4 rounded-md bg-gradient-to-r from-footerHeading to-parrot'>
+              <button className='mr-[60px] h-[40px] text-white w-full rounded-md bg-gradient-to-r from-footerHeading to-parrot'>
                 Confirm
               </button>
-              <button className='border mt-4 w-full h-[50px] rounded-md'>
+              <button className='border w-full h-[50px] rounded-md'>
                 <div className='flex justify-center'>
                   <img
                     className='inline px-3'
@@ -95,7 +101,7 @@ function Register() {
                   <h1 className='font-bold'> Continue with Google </h1>
                 </div>
               </button>
-              <h1 className='my-8'>
+              <h1 className='my-5'>
                 Already have an account?{' '}
                 <Link to='/login' className='text-footerHeading'>
                   Log in

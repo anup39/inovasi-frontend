a2a1f234bfbabf3dc887b2968919e8a0a0981d7f
arun <arcanearun@gmail.com>
2023-12-18 17:28:09 +0545

    responsiveness added

--- src/components/aboutuscomp/FAQ.tsx
+++ src/components/aboutuscomp/FAQ.tsx
@@ -15,10 +15,10 @@ function FAQ() {
 
   return (
     <div className="w-full h-full">
-      <h1 className="font-bold text-4xl text-center">
+      <h1 className="font-bold text-xl md:text-2xl lg:text-4xl text-center">
         Frequently Asked Questions
       </h1>
-      <div className="pt-[119px] flex flex-col gap-8">
+      <div className="pt-[70px] md:pt-[100px] lg:pt-[119px] flex flex-col gap-8">
         {accordionCount.map((index) => (
           <div key={index} className="">
             <div
@@ -26,14 +26,14 @@ function FAQ() {
                 activeAccordion === index
                   ? "bg-gradient-to-r from-footerHeading to-parrot rounded-t-[10px] text-white"
                   : "bg-faqBg bg-opacity-80 rounded-[10px]"
-              } text-[20px] h-[80px]`}
+              } text-[15px] md:text-[18px] lg:text-[20px] h-[80px]`}
             >
               <p className="font-semibold my-auto">
                 Lorem Ipsum is simply dummy
               </p>
               <svg
                 onClick={() => toggleAccordion(index)}
-                className={`cursor-pointer transition-all ${
+                className={`cursor-pointer transition-all scale-50 md:scale-75 lg:scale-100 ${
                   activeAccordion === index ? "rotate-180" : ""
                 }`}
                 width="30px"
@@ -50,9 +50,9 @@ function FAQ() {
             </div>
             {/* FAQ content opens up */}
             <div
-              className={`h-[170px] ${
+              className={`min-h-[170px] ${
                 activeAccordion === index ? "flex" : "hidden"
-              } items-center justify-center px-4 font-normal text-blackest leading-[30px] bg-white rounded-b-[10px]`}
+              } items-center justify-center px-4 font-normal text-[12px] md:text-[18px] text-blackest leading-[30px] bg-white rounded-b-[10px]`}
             >
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
